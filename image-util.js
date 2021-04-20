91c5c2bda870b360c146670e8804a112f7e710c6
Daniel <mwdchang@gmail.com>
2021-04-20 07:28:21 -0400

    mostly formatting

--- image-util.js
+++ image-util.js
@@ -1,5 +1,4 @@
 class ImageUtil {
-
   /**
    * Flattens a RGBA channel data into grey scale float array
    */
@@ -11,8 +10,8 @@ class ImageUtil {
 
     for (let i = 0; i < w * h; ++i) {
       const j = i * 4;
-      const newVal = (data[j+0] + data[j+1] + data[j+2] + data[j+3]) / 4.0 ;
-      flat.push( newVal / 255.0 );
+      const newVal = (data[j + 0] + data[j + 1] + data[j + 2] + data[j + 3]) / 4.0;
+      flat.push(newVal / 255.0);
     }
     return flat;
   }
@@ -25,7 +24,7 @@ class ImageUtil {
     const h = options.height || 0;
     const unflat = [];
     for (let i = 0; i < w * h; ++i) {
-      const val = data[i];
+      // const val = data[i];
       unflat.push(data[i] * 255);
       unflat.push(data[i] * 255);
       unflat.push(data[i] * 255);
@@ -39,13 +38,13 @@ class ImageUtil {
    */
   static crop(data, options, rect) {
     const w = options.width || 0;
-    const h = options.height || 0;
+    // const h = options.height || 0;
     const channels = options.channels;
-    const r = []
+    const r = [];
 
-    for (let i = rect.x; i < rect.x + rect.width; i++) {
-      for (let j = rect.y; j < rect.y + rect.height; j++) {
-        const idx = (i * w * channels) + j * channels;
+    for (let j = rect.y; j < rect.y + rect.height; j++) {
+      for (let i = rect.x; i < rect.x + rect.width; i++) {
+        const idx = (j * w * channels) + i * channels;
         r.push(data[idx]);
         r.push(data[idx + 1]);
         r.push(data[idx + 2]);
@@ -88,7 +87,7 @@ class ImageUtil {
    * @param {string} url - the image URL
    * @param {object} options
    * @param {number} options.width - optional, resize to specified width
-   * @param {number} options.height - optional, resize to specified height 
+   * @param {number} options.height - optional, resize to specified height
    *
    * FIXME: reuse canvas element if available
    * FIXME: add option for channel filters
@@ -101,6 +100,9 @@ class ImageUtil {
 
     const imgRequest = new Promise((resolve, reject) => {
       img.crossOrigin = '';
+      img.onerror = () => {
+        reject(new Error('unable to load image'));
+      };
       img.onload = () => {
         img.width = options.width || img.naturalWidth;
         img.height = options.height || img.naturalHeight;
@@ -108,7 +110,7 @@ class ImageUtil {
         canvas.width = img.width;
         canvas.height = img.height;
 
-        ctx.drawImage(img, 0 , 0, img.width, img.height);
+        ctx.drawImage(img, 0, 0, img.width, img.height);
         const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
         // ctx.drawImage(img, 0, i * chunkSize, img.width, chunkSize, 0, 0, img.width, chunkSize);
 
@@ -126,7 +128,7 @@ class ImageUtil {
   // Adapted from https://www.html5rocks.com/en/tutorials/canvas/imagefilters/
   static convolve(data, options, weights) {
     const side = Math.round(Math.sqrt(weights.length));
-    const halfSide = Math.floor(side/2);
+    const halfSide = Math.floor(side / 2);
     const sw = options.width;
     const sh = options.height;
     const channels = options.channels;
@@ -141,7 +143,7 @@ class ImageUtil {
       for (let x = 0; x < w; x++) {
         // calculate the weighed sum of the source image pixels that
         // fall under the convolution matrix
-        let ch = [];
+        const ch = [];
         for (let c = 0; c < channels; c++) {
           ch.push(0);
         }
@@ -158,8 +160,8 @@ class ImageUtil {
             const scy = y + cy - halfSide;
             const scx = x + cx - halfSide;
             if (scy >= 0 && scy < sh && scx >= 0 && scx < sw) {
-              let srcOff = (scy * sw + scx) * channels;
-              let wt = weights[cy * side + cx];
+              const srcOff = (scy * sw + scx) * channels;
+              const wt = weights[cy * side + cx];
 
               for (let c = 0; c < channels; c++) {
                 ch[c] += data[srcOff + c] * wt;
@@ -167,7 +169,7 @@ class ImageUtil {
             } else {
               // Not great ... but does the job
               for (let c = 0; c < channels; c++) {
-                ch[c] += data[(y * w + x) * channels] * ( 1/ weights.length) * 0.28;
+                ch[c] += data[(y * w + x) * channels] * (1 / weights.length) * 0.28;
               }
             }
           }
@@ -182,8 +184,8 @@ class ImageUtil {
 
   // Should use with channel = 1
   static sobel(data, options) {
-    const gx = [-1,  0,  1, -2, 0, 2, -1, 0, 1];
-         const gy = [-1, -2, -1,  0, 0, 0,  1, 2, 1];
+    const gx = [-1, 0, 1, -2, 0, 2, -1, 0, 1];
+    const gy = [-1, -2, -1, 0, 0, 0, 1, 2, 1];
 
     const gxResult = this.convolve(data, options, gx);
     const gyResult = this.convolve(data, options, gy);
@@ -194,7 +196,7 @@ class ImageUtil {
     // Join
     const r = [];
     for (let i = 0; i < gxResult.length; i++) {
-      const v = Math.sqrt( gxResult[i] * gxResult[i] + gyResult[i] * gyResult[i]);
+      const v = Math.sqrt(gxResult[i] * gxResult[i] + gyResult[i] * gyResult[i]);
       r.push(Math.min(1.0, v));
     }
     return r;
@@ -202,27 +204,26 @@ class ImageUtil {
 
 
   static blurGaussian = [
-    0.00000067,0.00002292,0.00019117,0.00038771,0.00019117,0.00002292,0.00000067,
-    0.00002292,0.00078633,0.00655965,0.01330373,0.00655965,0.00078633,0.00002292,
-    0.00019117,0.00655965,0.05472157,0.11098164,0.05472157,0.00655965,0.00019117,
-    0.00038771,0.01330373,0.11098164,0.22508352,0.11098164,0.01330373,0.00038771,
-    0.00019117,0.00655965,0.05472157,0.11098164,0.05472157,0.00655965,0.00019117,
-    0.00002292,0.00078633,0.00655965,0.01330373,0.00655965,0.00078633,0.00002292,
-    0.00000067,0.00002292,0.00019117,0.00038771,0.00019117,0.00002292,0.00000067
+    0.00000067, 0.00002292, 0.00019117, 0.00038771, 0.00019117, 0.00002292, 0.00000067,
+    0.00002292, 0.00078633, 0.00655965, 0.01330373, 0.00655965, 0.00078633, 0.00002292,
+    0.00019117, 0.00655965, 0.05472157, 0.11098164, 0.05472157, 0.00655965, 0.00019117,
+    0.00038771, 0.01330373, 0.11098164, 0.22508352, 0.11098164, 0.01330373, 0.00038771,
+    0.00019117, 0.00655965, 0.05472157, 0.11098164, 0.05472157, 0.00655965, 0.00019117,
+    0.00002292, 0.00078633, 0.00655965, 0.01330373, 0.00655965, 0.00078633, 0.00002292,
+    0.00000067, 0.00002292, 0.00019117, 0.00038771, 0.00019117, 0.00002292, 0.00000067
   ];
 
   static blur5x5 = [
-    1/25, 1/25, 1/25, 1/25, 1/25,
-    1/25, 1/25, 1/25, 1/25, 1/25,
-    1/25, 1/25, 1/25, 1/25, 1/25,
-    1/25, 1/25, 1/25, 1/25, 1/25,
-    1/25, 1/25, 1/25, 1/25, 1/25
+    1 / 25, 1 / 25, 1 / 25, 1 / 25, 1 / 25,
+    1 / 25, 1 / 25, 1 / 25, 1 / 25, 1 / 25,
+    1 / 25, 1 / 25, 1 / 25, 1 / 25, 1 / 25,
+    1 / 25, 1 / 25, 1 / 25, 1 / 25, 1 / 25,
+    1 / 25, 1 / 25, 1 / 25, 1 / 25, 1 / 25
   ];
 
   static blur3x3 = [
-    1/9, 1/9, 1/9,
-    1/9, 1/9, 1/9,
-    1/9, 1/9, 1/9 
+    1 / 9, 1 / 9, 1 / 9,
+    1 / 9, 1 / 9, 1 / 9,
+    1 / 9, 1 / 9, 1 / 9
   ];
-
 }
